5f7b45a52c3924a9f3d2033417d0175a37be5ae3
Tim Walkom <timwalkom@hotmail.com>
2025-06-30 17:15:16 +0930

    Created function for computers random choice

--- myScript.js
+++ myScript.js
@@ -1,1 +1,16 @@
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * (3) + 1);
+    if (num === 1) {
+        return 'Rock';
+    } else if (num === 2) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+
+function getPlayerChoice(choice) {
+
+}
+
 
